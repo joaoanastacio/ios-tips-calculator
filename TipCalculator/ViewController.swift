f3dece0c2e7cb215b48b0f00188d6e9902d70240
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-05-23 20:55:15 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Joao Victor Silva Anastacio on 2022-05-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
