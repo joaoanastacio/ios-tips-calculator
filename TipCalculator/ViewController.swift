0ef73c05cd840565d1cb4800564f97272c4c45da
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-05-23 23:54:06 -0700

    feature: add first implementation

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,12 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var billAmountTextField: UITextField!
+    @IBOutlet var tipIntroductionLabel: UILabel!
+    @IBOutlet var tipAmountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("Hello World, from Tip Calculator")
+        initializeUiElements()
     }
-
-
+    
+    private func initializeUiElements() {
+        tipIntroductionLabel.text = ""
+        tipAmountLabel.text = ""
+    }
+    
+    @IBAction func calculateTip(_ sender: UIButton) -> Void {
+        if isTextFieldEmpty() == false {
+            if billAmountTextField.text != nil {
+                tipIntroductionLabel.text = "You should be tipped in:"
+                tipAmountLabel.text = String(getTipAmountFromBill(bill: billAmountTextField.text!))
+            }
+        }
+        
+        // Case empty use one label to say that is required a value
+        print("Is Empty")
+    }
+    
+    private func isTextFieldEmpty() -> Bool {
+        if billAmountTextField.hasText {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    private func getTipAmountFromBill(bill billAmount: String) -> Double {
+        guard let billAmountConverted = Double(billAmount) else { return 0.0 }
+        return billAmountConverted * 0.15
+    }
 }
 
